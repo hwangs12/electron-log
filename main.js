39d561a6403d7283fe71c0f69423c75dce0fc279
hwangs12 <sungjun94@gmail.com>
2024-03-24 23:14:02 -0400

    chat app

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        backgroundColor: "#1F1A24",
         webPreferences: {
             preload: path.join(__dirname, "preload.js"),
         },
