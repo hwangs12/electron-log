5971315bda59ef1a673ed9944f2196c93cfd6e9f
hwangs12 <sungjun94@gmail.com>
2024-03-25 21:40:30 -0400

    handling error message

--- preload.js
+++ preload.js
@@ -4,7 +4,7 @@ contextBridge.exposeInMainWorld("versions", {
     node: () => process.versions.node,
     chrome: () => process.versions.chrome,
     electron: () => process.versions.electron,
-    ping: () => ipcRenderer.invoke("ping"),
+    sendError: () => ipcRenderer.send("open-error-dialog"),
     // we can also expose variables, not just functions
 });
 
--- renderer.js
+++ renderer.js
@@ -1,21 +1,18 @@
-import { ipcRenderer } from "electron";
-const ipc = ipcRenderer;
-
 const information = document.getElementById("info");
 information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`;
 
 const user = document.getElementById("user");
 user.innerText = environment.user();
 
-const func = async () => {
+/* const func = async () => {
     const response = await window.versions.ping();
     console.log(response);
-};
+}; */
 
 const errorBtn = document.getElementById("ipcButton");
 
 errorBtn.addEventListener("click", function () {
-    ipc.send("open-error-dialog");
+    window.versions.sendError();
 });
 
-func();
+// func();
