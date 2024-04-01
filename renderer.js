d9ce09028bb30036e68348f65071ac68735819e4
hwangs12 <sungjun94@gmail.com>
2024-03-31 22:08:59 -0400

    add button

--- renderer.js
+++ renderer.js
@@ -1,2 +1,18 @@
 const user = document.getElementById("user");
 user.innerText = environment.user();
+
+/**
+ * add chat preview on click
+ */
+function addChatPreview() {
+    var addChatButton = document.getElementById("add-chat");
+    var chatPreviewSection = document.getElementById("chat-preview-section");
+    var chatPreviewNode = document.createElement("div");
+    chatPreviewNode.className = "chat-preview";
+    chatPreviewNode.innerHTML += "yolo";
+    addChatButton.addEventListener("click", () => {
+        chatPreviewSection.appendChild(chatPreviewNode);
+    });
+}
+
+addChatPreview();
