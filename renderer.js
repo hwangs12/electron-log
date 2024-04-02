f47ace7d5fc21ab0cd015a11730340163321c893
hwangs12 <sungjun94@gmail.com>
2024-04-01 22:37:11 -0400

    some styling update

--- renderer.js
+++ renderer.js
@@ -1,18 +1,19 @@
 const user = document.getElementById("user");
 user.innerText = environment.user();
 
+/* add chat button */
+var addChatButton = document.getElementById("add-chat");
+
 /**
- * add chat preview on click
+ * function to add chat preview on click
  */
 function addChatPreview() {
-    var addChatButton = document.getElementById("add-chat");
     var chatPreviewSection = document.getElementById("chat-preview-section");
     var chatPreviewNode = document.createElement("div");
     chatPreviewNode.className = "chat-preview";
     chatPreviewNode.innerHTML += "yolo";
-    addChatButton.addEventListener("click", () => {
-        chatPreviewSection.appendChild(chatPreviewNode);
-    });
+    chatPreviewSection.appendChild(chatPreviewNode).cloneNode(true);
 }
 
-addChatPreview();
+/* add event listener on chat button */
+addChatButton.addEventListener("click", addChatPreview);
