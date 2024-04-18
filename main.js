ad7ded807f27dee22a11d998efea3b727d5ed74d
hwangs12 <sungjun94@gmail.com>
2024-04-17 21:13:18 -0400

    addchanel

--- main.js
+++ main.js
@@ -1,18 +1,6 @@
-const env = process.env.NODE_ENV || "development";
-const electron = require("electron");
-const dialog = electron.dialog;
-
-const { app, BrowserWindow, ipcMain } = require("electron");
-const path = require("node:path");
-
-let window;
-
-if (env === "development") {
-    require("electron-reload")(__dirname, {
-        electron: path.join(__dirname, "node_modules", ".bin", "electron"),
-        hardResetMethod: "exit",
-    });
-}
+const { app, BrowserWindow } = require("electron");
+const isDev = !app.isPackaged;
+const path = require("path");
 
 const createWindow = () => {
     /* you can create as many windows as you want */
@@ -30,19 +18,13 @@ const createWindow = () => {
     return window;
 };
 
-ipcMain.on("open-error-dialog", (event) => {
-    dialog.showErrorBox("An error message", "Demo of an error message");
-    event.sender.send(
-        "opened-error-dialog",
-        "Main process opened error dialog"
-    );
-    let window = new BrowserWindow();
-    window.loadURL("http://github.com");
-});
+if (isDev) {
+    require("electron-reload")(__dirname, {
+        electron: path.resolve(__dirname, "node_modules", ".bin", "electron"),
+    });
+}
 
-app.on("ready", () => {
-    window = createWindow();
-});
+app.whenReady().then(createWindow);
 
 app.on("window-all-closed", () => {
     if (process.platform !== "darwin") app.quit();
--- renderer.js
+++ renderer.js
@@ -7,12 +7,13 @@ var addChatButton = document.getElementById("add-chat");
 /**
  * function to add chat preview on click
  * author: Jun Hwang
+ * @version: 1.0
  */
 function addChatPreview() {
     var chatPreviewSection = document.getElementById("chat-preview-section");
     var chatPreviewNode = document.createElement("div");
     chatPreviewNode.className = "chat-preview";
-    chatPreviewNode.innerHTML += "yolo";
+    chatPreviewNode.innerHTML += "pikachu";
     chatPreviewSection.appendChild(chatPreviewNode).cloneNode(true);
 }
 
