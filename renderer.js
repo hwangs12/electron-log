2291f6ec8546afe3e338898d911dc0217ae6793a
hwangs12 <sungjun94@gmail.com>
2024-04-25 03:04:51 -0400

    refactor

--- renderer.js
+++ renderer.js
@@ -5,6 +5,7 @@ user.innerText = environment.user();
 
 /* add chat button */
 var addChatButton = document.getElementById("add-chat");
+var channel = document.getElementById("channel-manager-view");
 
 /**
  * function to add chat button on content loaded
@@ -35,8 +36,6 @@ function addChatPreview() {
  * @version: 1.0
  */
 function toggleAddChannel() {
-    var channel = document.getElementById("channel-manager-view");
-
     var addChannelInputForm = channel.childNodes[3];
 
     // confirmButtonElement.disabled = true;
@@ -45,19 +44,28 @@ function toggleAddChannel() {
         channel.removeChild(addChannelInputForm);
     } else {
         addChatButton.classList.add("rotate45");
-        var channelForm = document.createElement("form");
-        var fieldset = document.createElement("fieldset");
-        var inputElement = document.createElement("input");
-        inputElement.classList.add("channel-input");
-        var confirmButtonElement = document.createElement("button");
-        confirmButtonElement.classList.add("channel-submit");
-        confirmButtonElement.innerHTML = checkBox();
-        inputElement.setAttribute("placeholder", "Enter Channel Name");
-        fieldset.appendChild(inputElement);
-        fieldset.appendChild(confirmButtonElement);
-        channelForm.appendChild(fieldset);
-        channel.appendChild(channelForm);
+        createForm();
     }
+}
+
+/**
+ * function to create form
+ * author: Jun Hwang
+ * @version: 1.0
+ */
+function createForm() {
+    var channelForm = document.createElement("form");
+    var fieldset = document.createElement("fieldset");
+    var inputElement = document.createElement("input");
+    inputElement.classList.add("channel-input");
+    var confirmButtonElement = document.createElement("button");
+    confirmButtonElement.classList.add("channel-submit");
+    confirmButtonElement.innerHTML = checkBox();
+    inputElement.setAttribute("placeholder", "Enter Channel Name");
+    fieldset.appendChild(inputElement);
+    fieldset.appendChild(confirmButtonElement);
+    channelForm.appendChild(fieldset);
+    channel.appendChild(channelForm);
 }
 
 /**
