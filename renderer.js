dde4f556515be5acc789e6132d91d34561460900
hwangs12 <sungjun94@gmail.com>
2024-04-26 22:43:01 -0400

    adding event listener

--- renderer.js
+++ renderer.js
@@ -6,6 +6,7 @@ user.innerText = environment.user();
 /* add chat button */
 var addChatButton = document.getElementById("add-chat");
 var channel = document.getElementById("channel-manager-view");
+var confirmInputSubmitButton = document.getElementById("confirm-input-submit");
 
 /**
  * function to add chat button on content loaded
@@ -22,11 +23,11 @@ function addChatButtonOnLoad() {
  * author: Jun Hwang
  * @version: 1.0
  */
-function addChatPreview() {
+function addChatPreview(text) {
     var chatPreviewSection = document.getElementById("chat-preview-section");
     var chatPreviewNode = document.createElement("div");
     chatPreviewNode.className = "chat-preview";
-    chatPreviewNode.innerHTML += "pikachu";
+    chatPreviewNode.innerHTML += text ?? "";
     chatPreviewSection.appendChild(chatPreviewNode).cloneNode(true);
 }
 
@@ -58,10 +59,13 @@ function createForm() {
     var fieldset = document.createElement("fieldset");
     var inputElement = document.createElement("input");
     inputElement.classList.add("channel-input");
+    inputElement.setAttribute("name", "channel-name-input");
+    channelForm.setAttribute("id", "channel-add-form");
     var confirmButtonElement = document.createElement("button");
     confirmButtonElement.setAttribute("id", "confirm-input-submit");
     confirmButtonElement.classList.add("channel-submit");
     confirmButtonElement.innerHTML = checkBox();
+    inputElement.setAttribute("id", "channel-name-input");
     inputElement.setAttribute("placeholder", "Enter Channel Name");
     fieldset.appendChild(inputElement);
     fieldset.appendChild(confirmButtonElement);
@@ -80,8 +84,26 @@ function disableInputSubmitAtInit() {
     confirmInputSubmit.disabled = true;
 }
 
+/**
+ * function to submit the form
+ * author: Jun Hwang
+ * @version: 1.0
+ */
+function confirmSubmitAdd() {
+    let formdata = new FormData(document.getElementById("channel-add-form"));
+    let input = formdata.get("channel-name-input");
+    addChatPreview(input);
+}
+
 /* add event listener on document load */
 document.addEventListener("DOMContentLoaded", addChatButtonOnLoad);
 
 /* add event listener on add chat button */
 addChatButton.addEventListener("click", toggleAddChannel);
+
+document.body.addEventListener("submit", function (event) {
+    event.preventDefault();
+    if (event.target.id === "channel-add-form") {
+        confirmSubmitAdd();
+    }
+});
