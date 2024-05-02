019a8103c39449c79bc62f3ac62b1b0eeb26ac4a
hwangs12 <sungjun94@gmail.com>
2024-05-01 22:22:58 -0400

    main process to renderer

--- src/main.js
+++ src/main.js
@@ -2,6 +2,29 @@ const { app, BrowserWindow, ipcMain } = require("electron");
 const isDev = !app.isPackaged;
 const path = require("path");
 
+const { MongoClient } = require("mongodb");
+
+const uri = process.env.mongo_uri;
+console.log(uri);
+const client = new MongoClient(uri);
+
+async function run() {
+    try {
+        await client.connect();
+        const db = client.db("sample_mflix");
+        const collection = db.collection("movies");
+
+        // Find the first document in the collection
+        const first = await collection.findOne();
+        console.log(first);
+    } finally {
+        // Close the database connection when finished or an error occurs
+        await client.close();
+    }
+}
+
+run().catch(console.error);
+
 const createWindow = () => {
     /* you can create as many windows as you want */
     const window = new BrowserWindow({
