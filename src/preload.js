fb5e7113eeaf0f4bd3c4624e0abe14318c31092e
hwangs12 <sungjun94@gmail.com>
2024-06-01 22:10:32 -0400

    unused preload

--- src/preload.js
+++ src/preload.js
@@ -40,33 +40,3 @@ const ipc = {
         sendReceive: [],
     },
 };
-
-// Exposed protected methods in the render process.
-contextBridge.exposeInMainWorld(
-    // Allowed 'ipcRenderer' methods.
-    "ipcRender",
-    {
-        // From render to main.
-        send: (channel, args) => {
-            let validChannels = ipc.render.send;
-            if (validChannels.includes(channel)) {
-                ipcRenderer.send(channel, args);
-            }
-        },
-        // From main to render.
-        receive: (channel, listener) => {
-            let validChannels = ipc.render.receive;
-            if (validChannels.includes(channel)) {
-                // Deliberately strip event as it includes `sender`.
-                ipcRenderer.on(channel, (event, ...args) => listener(...args));
-            }
-        },
-        // From render to main and back again.
-        invoke: (channel, args) => {
-            let validChannels = ipc.render.sendReceive;
-            if (validChannels.includes(channel)) {
-                return ipcRenderer.invoke(channel, args);
-            }
-        },
-    }
-);
